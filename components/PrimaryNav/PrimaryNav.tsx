393b9ae5ac5067ed3f2515c8b980ffee9c94d358
Chris DeMars <cdemars86@gmail.com>
2025-05-29 00:58:58 -0400

    more stuff

--- components/PrimaryNav/PrimaryNav.tsx
+++ components/PrimaryNav/PrimaryNav.tsx
@@ -1,7 +1,28 @@
-export default function PrimaryNav() {
-  return (
-    <div>
+import Link from "next/link";
 
-    </div>
+const navItems = [
+  { name: "Home", href: "/" },
+  { name: "About", href: "/about" },
+  { name: "Tournaments", href: "/tournaments" },
+  { name: "Contact", href: "/contact" }
+];
+
+export default function PrimaryNav() {
+
+  return (
+    <nav>
+      <ul className="flex space-x-4">
+        {navItems.map((item) => (
+          <li key={item.name}>
+            <Link
+              href={item.href}
+              className="text-white hover:text-fishing-lime transition-colors duration-300 font-medium"
+            >
+              {item.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
   );
 }
